4ec804e48dad047768eff568b87c3585ffe69c21
孟凡 <DYA1227@163.com>
2017-04-19 19:45:46 +0800

    Initial Commit

---
+++ NameList/AppDelegate.h
@@ -0,0 +1,17 @@
+//
+//  AppDelegate.h
+//  NameList
+//
+//  Created by mengdy on 17/4/19.
+//  Copyright © 2017年 mengdy. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface AppDelegate : UIResponder <UIApplicationDelegate>
+
+@property (strong, nonatomic) UIWindow *window;
+
+
+@end
+
---
+++ NameList/ViewController.h
@@ -0,0 +1,15 @@
+//
+//  ViewController.h
+//  NameList
+//
+//  Created by mengdy on 17/4/19.
+//  Copyright © 2017年 mengdy. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface ViewController : UIViewController
+
+
+@end
+
